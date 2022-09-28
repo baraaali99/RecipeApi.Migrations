ad90b3631573891a67621b004c25748f03fa2845
Baraa Alfaris <baraa.ali1912@gmail.com>
2022-09-28 23:25:55 +0400

    Add project files.

---
+++ RecipeApi.Migrations/Migrations/002_CreateTableCategory.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+namespace RecipeApi.Migrations.Migrations
+{
+    [Migration(2)]
+    public class _002_CreateTableCategory : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Category")
+           .WithColumn("name").AsString()
+           .WithColumn("IsActive").AsBoolean().NotNullable().WithDefaultValue(true);
+
+            Create.ForeignKey()
+           .FromTable("Ingredient").ForeignColumn("name")
+           .ToTable("Category");
+        }
+    }
+}
---
+++ RecipeApi.Migrations/Migrations/003_CreateTableUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+namespace RecipeApi.Migrations.Migrations
+{
+    [Migration(3)]
+    public class _003_CreateTableUser : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("User")
+            .WithColumn("Username").AsString().PrimaryKey()
+            .WithColumn("Password").AsString().NotNullable()
+            .WithColumn("RefreshToken").AsString().Unique();
+        }
+    }
+}
